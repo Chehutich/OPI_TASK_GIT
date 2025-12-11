ccd91f50dcc856e05d02df01234c36d8ec67f445
Chehutich <disomikvlog@gmail.com>
2025-12-11 20:48:58 +0100

    backend // V0.3 // rework datemanager

--- OPI_TASK_GIT/Services/DataManager.cs
+++ OPI_TASK_GIT/Services/DataManager.cs
@@ -78,28 +78,7 @@ namespace QuizApp.Services
             geo.Questions.Add(new Question("Столиця Великої Британії?", new List<string> { "Лондон", "Дублін", "Единбург", "Манчестер" }, 0));
             geo.Questions.Add(new Question("Де знаходяться піраміди?", new List<string> { "Мексика", "Індія", "Єгипет", "Китай" }, 2));
 
-            // Тест 2: Історія України
-            Quiz hist = new Quiz
-            {
-                Title = "Історія України",
-                Category = "Історія",
-                Description = "Основні дати та видатні постаті."
-            };
-
-            hist.Questions.Add(new Question("Коли була проголошена незалежність України?", new List<string> { "1990", "1991", "1996", "2004" }, 1));
-            hist.Questions.Add(new Question("В якому році хрестили Русь?", new List<string> { "988", "1054", "1240", "882" }, 0));
-            hist.Questions.Add(new Question("Хто був першим президентом України?", new List<string> { "Кучма", "Ющенко", "Кравчук", "Порошенко" }, 2));
-            hist.Questions.Add(new Question("Коли прийняли Конституцію України?", new List<string> { "1991", "1996", "2000", "2014" }, 1));
-            hist.Questions.Add(new Question("Гетьман, зображений на купюрі 5 гривень?", new List<string> { "Мазепа", "Хмельницький", "Сагайдачний", "Виговський" }, 1));
-            hist.Questions.Add(new Question("Столиця Київської Русі?", new List<string> { "Чернігів", "Галич", "Київ", "Львів" }, 2));
-            hist.Questions.Add(new Question("В якому році сталася аварія на ЧАЕС?", new List<string> { "1980", "1986", "1991", "2000" }, 1));
-            hist.Questions.Add(new Question("Автор 'Кобзаря'?", new List<string> { "Іван Франко", "Леся Українка", "Тарас Шевченко", "Григорій Сковорода" }, 2));
-            hist.Questions.Add(new Question("Як називалась козацька держава?", new List<string> { "Гетьманщина", "Запорізька Січ", "УНР", "Русь" }, 1));
-            hist.Questions.Add(new Question("Грошова одиниця України?", new List<string> { "Рубль", "Долар", "Гривня", "Злотий" }, 2));
-
-            Quizzes.Add(geo);
-            Quizzes.Add(hist);
-            SaveQuizzes();
+            
         }
     }
 }
